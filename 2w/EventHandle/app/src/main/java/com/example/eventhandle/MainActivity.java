3c3ee6715f63e036ef26a02cb73c88c9b040f756
KSH <40918958+gitksh@users.noreply.github.com>
2022-09-15 16:02:27 +0900

    2w

---
+++ 2w/EventHandle/app/src/main/java/com/example/eventhandle/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.eventhandle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView1 = findViewById(R.id.textview1);
+        textView1.setOnClickListener(v -> textView1.setBackgroundColor(Color.GREEN));
+
+    }
+}
